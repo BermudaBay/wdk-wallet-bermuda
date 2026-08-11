ac41a966ceca55e15fb3caa1f5199553485614cd
Yugo <yuu5infinity@gmail.com>
2026-08-11 15:31:58 +0200

    chore: update Bermuda SDK to v0.1.7

--- tests/wallet-manager-bermuda.test.js
+++ tests/wallet-manager-bermuda.test.js
@@ -28,15 +28,15 @@ describe('WalletManagerBermuda', () => {
 
       expect(account).toBeInstanceOf(WalletAccountBermuda)
 
-      expect(account.address).toMatch(/^0x[0-9a-f]{128}$/)
+      expect(account.address).toMatch(/^0x[0-9a-f]{448}$/)
     })
 
     test('should return another Bermuda sub account', async () => {
       const account00 = await wallet.getBermudaAccount(0, 0)
       const account01 = await wallet.getBermudaAccount(0, 1)
 
-      expect(account00.address).toMatch(/^0x[0-9a-f]{128}$/)
-      expect(account01.address).toMatch(/^0x[0-9a-f]{128}$/)
+      expect(account00.address).toMatch(/^0x[0-9a-f]{448}$/)
+      expect(account01.address).toMatch(/^0x[0-9a-f]{448}$/)
       expect(account00.address).not.toEqual(account01.address)
     })
 
